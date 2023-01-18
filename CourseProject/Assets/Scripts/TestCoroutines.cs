1c6cd35ae252c0374d3b6a6d820e3ecdfcdee423
Joël Mbiapa <94489918+Personne73@users.noreply.github.com>
2023-01-18 15:09:05 +0100

    fin cours coroutine

--- CourseProject/Assets/Scripts/TestCoroutines.cs
+++ CourseProject/Assets/Scripts/TestCoroutines.cs
@@ -2,19 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+ 
+
 public class TestCoroutines : MonoBehaviour
 {
+    [SerializeField] Transform[] m_WayPoints;
+    IEnumerator m_MyCoroutine;
+    
     // Coroutine : méthode à l'intérieur de laquelle on peut sortir et mettre en pause la méthode puis y revenir plus tard
+    // Start is called before the first frame update
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitCoroutine(2));
-    }
+        //StartCoroutine(WaitCoroutine(2));
+        //StartCoroutine(UpdateCoroutineIA());
+        //StartCoroutine(UpdateCoroutineGameLogic());
+        //StartCoroutine(MyTools.TranslationCoroutine(transform,transform.position, transform.position + Random.onUnitSphere * 6, 2));
 
+    }
+    
+    private void Update()
+    {
+        MyTools.LOG(this, "UPDATE");
+    }
+    
     IEnumerator WaitCoroutine(float waitDuration)
     {
         Debug.Log("START WaitCoroutine");
         yield return new WaitForSeconds(waitDuration);
         Debug.Log("END WaitCoroutine");
+    }
+    
+    IEnumerator UpdateCoroutineIA()
+    {
+        MyTools.LOG(this, "Start UpdateCoroutineIA");
+        while (true)
+        {
+            MyTools.LOG(this, "Update UpdateCoroutineIA");
+            yield return null; // atends la prochaine frame
+        }
+        MyTools.LOG(this, "End UpdateCoroutineIA");
+
+        yield break; // return violent
+    }
+
+    IEnumerator UpdateCoroutineGameLogic()
+    {
+        MyTools.LOG(this, "Start UpdateCoroutineGameLogic");
+        while (true)
+        {
+            MyTools.LOG(this, "Update UpdateCoroutineGameLogic");
+            yield return null; // atends la prochaine frame
+        }
+        MyTools.LOG(this, "End UpdateCoroutineGameLogic");
+        
+        yield break; // return violent
+    }
+    
+    IEnumerator PatrolCoroutine(Transform[] wayPoints, float translationSpeed)
+    {
+        int indexWayPoint = 0;
+        
+        while(true)
+        {
+            yield return StartCoroutine(MyTools.TranslationCoroutine(transform,wayPoints[indexWayPoint % wayPoints.Length].position,
+                                                            wayPoints[(indexWayPoint + 1) % wayPoints.Length].position,
+                                                            translationSpeed));
+            
+            yield return new WaitForSeconds(1);
+            indexWayPoint++;
+        }
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 200, 40), "STOP COROUTINE"))
+        {
+            StopCoroutine(m_MyCoroutine);
+            m_MyCoroutine = null;
+        }
+        if (GUI.Button(new Rect(10, 50, 200, 40), "STOP ALL COROUTINES"))
+        {
+            StopAllCoroutines();
+            m_MyCoroutine = null;
+        }
+        if (GUI.Button(new Rect(10, 110, 200, 40), "START COROUTINE"))
+        {
+            m_MyCoroutine = PatrolCoroutine(m_WayPoints, 4); // Closure ... attention la méthode n'est pas appelée ici
+            StartCoroutine(m_MyCoroutine);
+        }
+        
     }
 }
